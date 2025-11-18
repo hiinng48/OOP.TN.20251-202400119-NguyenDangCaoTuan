11359b0e96dff4402bfa0a187bda53dcb6fc2431
hiinng48 <hiinnguyen448@outlook.com>
2025-11-18 19:04:38 +0700

    update old code

--- AimsProject/src/com/hust/kstn/Aims.java
+++ AimsProject/src/com/hust/kstn/Aims.java
@@ -1,5 +1,21 @@
 package com.hust.kstn;
 
+import com.hust.kstn.models.*;
+
 public class Aims {
-        
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        anOrder.print();
+
+        anOrder.removeDigitalVideoDisc(dvd2);
+        anOrder.print();
+    }
 }
